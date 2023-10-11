5c645df32de4acdac80eb3ccf98cbcbda6cd3783
Yongjin Park <ypp@stat.ubc.ca>
2023-10-11 07:07:43 -0700

    update conditional qtl

--- script/call_iqtl_per_ld.R
+++ script/call_iqtl_per_ld.R
@@ -5,19 +5,23 @@ options(stringsAsFactors = FALSE)
 ## ld.index <- 1609
 ## geno.hdr <- "result/step4/rosmap"
 ## expr.file <- "result/step3/log_mean.bed.gz"
+## herit.dir <- "result/step4/heritability/"
 ## svd.file <- "result/step3/svd.rds"
 ## pheno.file <- "data/metadata_PFC_all_individuals_092520.tsv.gz"
+## max.K <- 37
 ## out.file <- "output.txt.gz"
 
-if(length(argv) < 7) q()
+if(length(argv) < 9) q()
 
 ld.index <- as.integer(argv[1])
 ld.file <- argv[2]
 geno.hdr <- argv[3]
 expr.file <- argv[4]
-svd.file <- argv[5]
-pheno.file <- argv[6]
-out.file <- argv[7]
+herit.dir <- argv[5]
+svd.file <- argv[6]
+pheno.file <- argv[7]
+max.K <- argv[8]
+out.file <- argv[9]
 
 temp.dir <- paste0(out.file, "_temp")
 
@@ -99,6 +103,57 @@ match.with.plink <- function(Y, plink){
     return(ret)
 }
 
+take.marginal.stat <- function(xx, yy, se.min=1e-8) {
+    .xx <- apply(xx, 2, scale)
+    .yy <- apply(yy, 2, scale)
+    rm.na.zero <- function(xx) {
+        return(replace(xx, is.na(xx), 0))
+    }
+    n.obs <- crossprod(!is.na(.xx), !is.na(.yy))
+    beta.mat <- crossprod(rm.na.zero(.xx), rm.na.zero(.yy))/n.obs
+    ## calibrate residuals
+    resid.se.mat <- matrix(NA, ncol(.xx), ncol(.yy))
+    for (k in 1:ncol(.yy)) {
+        beta.k <- beta.mat[, k]
+        yy.k <- .yy[, k]
+        err.k <- sweep(sweep(.xx, 2, beta.k, `*`), 1, yy.k, `-`)
+        se.k <- apply(err.k, 2, sd, na.rm = TRUE)
+        resid.se.mat[, k] <- se.k + se.min
+    }
+    y.cols <- 1:ncol(yy)
+    colnames(beta.mat) <- y.cols
+    colnames(n.obs) <- y.cols
+    colnames(resid.se.mat) <- y.cols
+    rownames(resid.se.mat) <- rownames(beta.mat)
+    ## combine the results
+    .melt.mat <- function(.mat, ...) {
+        rownames(.mat) <- 1:nrow(.mat)
+        colnames(.mat) <- 1:ncol(.mat)
+        reshape2::melt(.mat, ...) %>%
+            dplyr::rename(x.col = Var1, y.col = Var2) %>%
+            as.data.table()
+    }
+    zscore.pvalue <- function(z) {
+        2*pnorm(abs(z), lower.tail = FALSE)
+    }
+    beta.dt <- .melt.mat(beta.mat, value.name = "beta")
+    resid.se.dt <- .melt.mat(resid.se.mat, value.name = "resid.se")
+    nobs.dt <- .melt.mat(n.obs, value.name = "n")
+    out <- beta.dt %>%
+        left_join(nobs.dt, by = c("x.col", "y.col")) %>%
+        left_join(resid.se.dt, by = c("x.col", "y.col")) %>%
+        dplyr::mutate(se = resid.se/sqrt(n)) %>%
+        dplyr::mutate(p.val = zscore.pvalue(beta/se)) %>%
+        dplyr::mutate(beta = round(beta, 4)) %>%
+        dplyr::select(-resid.se) %>%
+        dplyr::mutate(se = round(se, 4)) %>%
+        dplyr::mutate(x.col = as.integer(x.col)) %>%
+        dplyr::mutate(y.col = as.integer(y.col)) %>%
+        as.data.table()
+
+    return(out)
+}
+
 .quantile.norm <- function(.mat) {
     stopifnot(is.matrix(.mat))
     ret <- .mat
@@ -115,6 +170,108 @@ match.with.plink <- function(Y, plink){
     rownames(ret) <- rownames(.mat)
     colnames(ret) <- colnames(.mat)
     return(ret)
+}
+
+fast.z <- function (x, y)
+{
+    n.obs <- crossprod(!is.na(x), !is.na(y))
+    ret <- crossprod(replace(x, is.na(x), 0), replace(y, is.na(y), 0))/sqrt(n.obs)
+    ret[is.na(ret)] <- 0
+    return(ret)
+}
+
+svd.theta <- function(.svd, k, zz){
+    lambda <- 1/nrow(.svd$u)
+    vd2inv <- sweep(.svd$v[, 1:k, drop = F], 2, 1/(.svd$d[1:k]^2 + lambda), `*`)
+    vz.k <- t(.svd$v[, 1:k, drop = F]) %*% zz
+    .theta.k <- vd2inv %*% vz.k
+}
+
+svd.pgs <- function(.svd, k, zz){
+    lambda <- 1/nrow(.svd$u)
+    udinv.k <- sweep(.svd$u[, 1:k, drop = F], 2, 1/(.svd$d[1:k] + sqrt(lambda)), `*`)
+    vz.k <- t(.svd$v[, 1:k, drop = F]) %*% zz
+    udinv.k %*% vz.k
+}
+
+################################################################
+
+safe.scale <- function(.mat){
+    ret <- apply(.mat, 2, scale)
+    ret[is.na(ret)] <- 0
+    ret
+}
+
+################################################################
+
+adj.by.svd <- function(.data){
+
+    imp.r <- function(r){
+        ww <- .data$w[,r]
+
+        x0 <- .data$x[ww == 0, , drop = F]
+        y0 <- .data$y[ww == 0, , drop = F]
+        x1 <- .data$x[ww == 1, , drop = F]
+        y1 <- .data$y[ww == 1, , drop = F]
+
+        z1 <- fast.z(x1, y1)
+        z0 <- fast.z(x0, y0)
+
+        .svd1 <- rsvd::rsvd(x1, k=max(.data$svd.k))
+        .svd0 <- rsvd::rsvd(x0, k=max(.data$svd.k))
+
+
+        y0.for.w1 <- sapply(1:length(.data$svd.k),
+                            function(j) svd.pgs(.svd1,
+                                                .data$svd.k[j],
+                                                z0[,j]))
+
+        y1.for.w0 <- sapply(1:length(.data$svd.k),
+                            function(j) svd.pgs(.svd0,
+                                                .data$svd.k[j],
+                                                z1[,j]))
+
+        y1.adj <- sapply(1:ncol(y1), function(j){
+            .safe.lm(y1[,j,drop=F], y0.for.w1[,j,drop=F])$residuals
+        })
+
+        y0.adj <- sapply(1:ncol(y1), function(j){
+            .safe.lm(y0[,j,drop=F], y1.for.w0[,j,drop=F])$residuals
+        })
+
+        list(y0 = y0.adj, y1 = y1.adj, x0 = x0, x1 = x1)
+    }
+
+    ret <- lapply(1:ncol(.data$w), imp.r)
+    names(ret) <- colnames(.data$w)
+    return(ret)
+}
+
+################################################################
+
+take.susie.cs <- function(xx,yy){
+    susie <- mtSusie::mt_susie(X = xx,
+                               Y = yy,
+                               L = 15,
+                               prior.var = .1,
+                               coverage = .9,
+                               output.full.stat = F,
+                               local.residual = T)
+
+    susie.dt <-
+        data.table::setDT(susie$cs) %>%
+        dplyr::rename(x.col = variants) %>%
+        dplyr::rename(y.col = traits) %>%
+        na.omit() %>%
+        as.data.table()
+
+    marg.stat <- take.marginal.stat(xx,yy)
+
+    ret <- left_join(susie.dt, marg.stat)
+
+    ret[order(`p.val`),
+        head(.SD, 1),
+        by = .(x.col, y.col)]
 }
 
 ################################################################
@@ -136,12 +293,17 @@ unlink(temp.dir, recursive=TRUE)
 .cmd <- "tabix " %&% expr.file %&% " " %&% .query %&% " " %&% " -h"
 expr.dt <- fread(cmd=.cmd, sep="\t", header=T)
 
+herit.file <- herit.dir %&% "/PC" %&% max.K %&% "/" %&% ld.index %&% ".txt.gz"
+herit.dt <- fread(herit.file)
+
 pheno.dt <- fread(pheno.file, header = T)
-pheno.dt[, var_amyloid := scale(sqrt(`amyloid`))]
-pheno.dt[, var_nft := scale(sqrt(`nft`))]
-pheno.dt[, var_msex := scale(`msex`)]
-pheno.dt[, var_age := scale(`age_death`)]
-pheno.dt[, var_braaksc := scale(`braaksc`)]
+
+## pheno.dt[, var_braaksc := scale(`braaksc`)]
+## pheno.dt[, var_amyloid := scale(sqrt(`amyloid`))]
+## pheno.dt[, var_nft := scale(sqrt(`nft`))]
+## pheno.dt[, var_msex := scale(`msex`)]
+## pheno.dt[, var_age := scale(`age_death`)]
+## pheno.dt[, var_braaksc := scale(`braaksc`)]
 
 message("read all the data")
 
@@ -149,9 +311,9 @@ genes <- unique(expr.dt$hgnc_symbol)
 
 output <- data.table()
 
-for(gene in genes){
+for(g in genes){
 
-    .temp <- expr.dt[hgnc_symbol == gene]
+    .temp <- expr.dt[hgnc_symbol == g]
 
     y.ct <- .temp$celltype
     Y <- as.matrix(t(.temp[, -(1:6)]))
@@ -163,22 +325,13 @@ for(gene in genes){
 
     Y <- Y[, observed >= .10, drop = F]
 
-    W <- pheno.dt[match(rownames(Y), `projid`),
-                  .(var_amyloid,
-                    var_nft,
-                    var_msex,
-                    var_age,
-                    var_braaksc)] %>% 
-        as.matrix()
-
-    rownames(W) <- rownames(Y)
-
-    covar <- apply(.svd.covar$u[rownames(Y), ], 2, scale)
+    #################################
+    ## Adjust technical covariates ##
+    #################################
+    covar <- apply(.svd.covar$u[rownames(Y), 1:max.K, drop = F], 2, scale)
     .lm <- .safe.lm(Y, covar)
-
     .data <- match.with.plink(.lm$residuals, plink)
-    .data$x <- apply(.data$x, 2, scale)
-    .data$w <- W[match(rownames(.data$y), rownames(W)), , drop = F]
+    .data$x <- safe.scale(.data$x)
 
     x.dt <- as.data.table(plink$map)
     x.dt <- x.dt[, .(`chromosome`,
@@ -187,43 +340,46 @@ for(gene in genes){
                      `allele2`)] %>%
         cbind(x.col=1:ncol(.data$x))
 
-    w.dt <- data.table(pheno = gsub("var_","",colnames(W))) %>%
-        mutate(w.col = 1:n())
-
     y.dt <- data.table(celltype=colnames(.data$y), y.col=1:ncol(.data$y))
 
-    susie <- mtSusie::mt_susie(X = .data$x,
-                               Y = .data$y,
-                               W = .data$w,
-                               L = 1,
-                               prior.var = .01,
-                               coverage = .9,
-                               output.full.stat = F)
+    ################
+    ## conditions ##
+    ################
+    W <- pheno.dt[match(rownames(.data$y), `projid`),
+                  .(projid, msex, niareagansc)]
 
-    susie.dt <-
-        setDT(susie$cs) %>%
-        dplyr::rename(x.col = variants) %>%
-        dplyr::rename(y.col = traits) %>%
-        dplyr::rename(w.col = interaction) %>%
-        na.omit() %>%
-        filter(lodds > 0, w.col > 0) %>%
-        as.data.table()
+    W[, AD := 0]
+    W[`niareagansc` <= 2, AD := 1]
+    .data$w <- as.matrix(W[, .(msex, AD)])
 
-   susie.best <-
-        susie.dt[order(abs(`z`), decreasing = T),
-                 head(.SD, 1),
-                 by = .(x.col, y.col)]
+    ######################
+    ## optimal SVD rank ##
+    ######################
 
-    .out <- susie.best %>%
+    .data$svd.k <- herit.dt[gene == g][match(colnames(.data$y),`celltype`)]$svd.k
+
+    cond.data <- adj.by.svd(.data)
+
+    .out <- data.table()
+
+    for(ii in 1:length(cond.data)){
+        .w <- names(cond.data)[ii]
+        .dt1 <- take.susie.cs(cond.data[[ii]]$x1, cond.data[[ii]]$y1)
+        .dt0 <- take.susie.cs(cond.data[[ii]]$x0, cond.data[[ii]]$y0)
+        .dt1[, W := paste0(.w, 1)]
+        .dt0[, W := paste0(.w, 0)]        
+        .out <- rbind(rbind(.out, .dt1), .dt0)
+    }
+
+    .out <- .out %>%
         left_join(x.dt) %>% 
         left_join(y.dt) %>% 
-        left_join(w.dt) %>% 
-        dplyr::select(-y.col, -x.col, -w.col) %>%
-        dplyr::mutate(gene) %>%
+        dplyr::select(-y.col, -x.col) %>%
+        dplyr::mutate(gene = g) %>%
         na.omit() %>%
         as.data.table()
 
-    message("Computed: ", gene)
+    message("Computed: ", g)
 
     output <- rbind(output, .out)
 }
@@ -234,12 +390,12 @@ if(nrow(output) < 1){
 
     out.dt <-
         output %>%
-        dplyr::select(`chromosome`, `physical.pos`, `pheno`,
+        dplyr::select(`chromosome`, `physical.pos`, `W`,
                       `gene`, `celltype`,
-                      `alpha`, `mean`, `var`, `lbf`, `z`, `lodds`, `lfsr`) %>%
+                      `alpha`, `mean`, `sd`, `lbf`, `z`, `lodds`, `lfsr`,
+                      `p.val`) %>%
         dplyr::mutate(`alpha` = round(`alpha`, 4),
                       `mean` = round(`mean`, 4),
-                      `var` = round(`var`, 4),
                       `lbf` = round(`lbf`, 4),
                       `z` = round(`z`, 4),
                       `lodds` = round(`lodds`, 4),
